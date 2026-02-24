81844efaee8590c0aa24fe6dbb0e3a79c3a75f06
yashmishraaaa <yashmishramishra401@gmail.com>
2026-02-24 18:19:30 +0530

    demo change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Hello from EC2 CI/CD 🚀");
+  res.end("Hello from Advanced CI/CD to EC2");
 });
 
 server.listen(3000, "0.0.0.0");
